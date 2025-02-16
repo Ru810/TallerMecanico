ad42659aaa0ffb937abd90159c9b432e80f67c45
Adrian <rubioadrian@proton.me>
2025-02-16 23:16:35 +0100

    Commit inicial y con los archivos Cliente,Revision y Vehiculo.

--- src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/dominio/Cliente.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/dominio/Cliente.java
@@ -1,34 +1,27 @@
 package org.iesalandalus.programacion.tallermecanico.modelo.dominio;
-
 import java.util.Objects;
 
 public class Cliente {
     private static final String ER_NOMBRE = "^(?u)[A-ZÁÉÍÓÚÑ][a-záéíóúñ]+(?: [A-ZÁÉÍÓÚÑ][a-záéíóúñ]+)*$";
     private static String ER_DNI = "\\d{8}[A-Z]";
     private static String ER_TELEFONO = "\\d{9}";
-
     private String nombre;
     private String dni;
     private String telefono;
-
     public Cliente(String nombre, String dni, String telefono) {
         setNombre(nombre);
         setDni(dni);
         setTelefono(telefono);
     }
-
     public Cliente (Cliente cliente) {
         Objects.requireNonNull(cliente, "No es posible copiar un cliente nulo.");
         this.nombre = cliente.nombre;
         this.dni = cliente.dni;
         this.telefono = cliente.telefono;
     }
-
-
     public String getNombre() {
         return nombre;
     }
-
     public void setNombre(String nombre) {
         Objects.requireNonNull(nombre, "El nombre no puede ser nulo.");
         if (!nombre.matches(ER_NOMBRE)) {
@@ -36,11 +29,9 @@ public class Cliente {
         }
         this.nombre = nombre;
     }
-
     public String getDni() {
         return dni;
     }
-
     private void setDni(String dni) {
         Objects.requireNonNull(dni, "El DNI no puede ser nulo.");
         if (!dni.matches(ER_DNI)) {
@@ -51,18 +42,15 @@ public class Cliente {
         }
         this.dni = dni;
     }
-
     private boolean comprobarLetraDni(String dni) {
         String letrasDni = "TRWAGMYFPDXBNJZSQVHLCKE";
         int numero = Integer.parseInt(dni.substring(0, 8));
         char letraCalculada = letrasDni.charAt(numero % 23);
         return dni.charAt(8) == letraCalculada;
     }
-
     public String getTelefono() {
         return telefono;
     }
-
     public void setTelefono(String telefono) {
         Objects.requireNonNull(telefono, "El teléfono no puede ser nulo.");
         if (!telefono.matches(ER_TELEFONO)) {
@@ -70,7 +58,6 @@ public class Cliente {
         }
         this.telefono = telefono;
     }
-
     public static Cliente get(String dni) {
         Objects.requireNonNull(dni, "El DNI no puede ser nulo.");
         if (!dni.matches(ER_DNI)) {
@@ -84,7 +71,6 @@ public class Cliente {
         }
         return new Cliente("", dni, "");
     }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -92,12 +78,10 @@ public class Cliente {
         Cliente cliente = (Cliente) o;
         return Objects.equals(dni, cliente.dni);
     }
-
     @Override
     public int hashCode() {
         return Objects.hash(dni);
     }
-
     @Override
     public String toString() {
         return String.format("%s - %s (%s)", nombre, dni, telefono);
